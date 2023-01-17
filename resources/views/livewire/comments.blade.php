fd2799544fbc1af43ecb38cf57030b862fb68e8f
salman_rahman <salmanrahman.cse@gmail.com>
2023-01-17 13:01:39 +0600

    comment form with lazy

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,13 +1,13 @@
 <div class="flex justify-center">
     <div class="w-6/12">
         <h1 class="my-10 text-3xl">Comments</h1>
-        <div class="my-4 flex">
+        <form class="my-4 flex" wire:submit.prevent='addComment'>
             <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="What is your Mind.."
-                wire:model="newComment">
+                wire:model.lazy="newComment">
             <div class="py-2">
-                <button class="p-2 bg-blue-500 w-20 rounded shadow text-white" wire:click='addComment'>Add</button>
+                <button class="p-2 bg-blue-500 w-20 rounded shadow text-white" type="submit">Add</button>
             </div>
-        </div>
+        </form>
         @foreach ($comments as $comment)
             <div class="rounded border shadow p-3 my-2">
                 <div class="flex justify-start my-2">
