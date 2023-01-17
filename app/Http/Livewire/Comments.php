4ec67615ac396d8c40e313274f323ea7fac8a3c8
salman_rahman <salmanrahman.cse@gmail.com>
2023-01-17 17:58:03 +0600

    comments pagination

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -5,18 +5,16 @@ namespace App\Http\Livewire;
 use App\Models\Comment;
 use Carbon\Carbon;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Comments extends Component
 {
-    public $comments;
+    use WithPagination;
+    // public $comments;
 
     public $newComment;
 
-    public function mount($initialComments)
-    {
-        $initialComments = Comment::orderBy('id', 'DESC')->get();
-        $this->comments = $initialComments;
-    }
+
 
     public function updated($body)
     {
@@ -38,7 +36,7 @@ class Comments extends Component
             'body' => $this->newComment,
             'user_id' => 1,
         ]);
-        $this->comments->prepend($createdComment);
+        // $this->comments->prepend($createdComment);
         $this->newComment = '';
         session()->flash('message', 'Comment successfully Added.');
     }
@@ -46,12 +44,14 @@ class Comments extends Component
     public function remove($commentId)
     {
         $comment = Comment::find($commentId);
-        $this->comments = $this->comments->except($commentId);
-        session()->flash('message', 'Comment deleted Successfully.');
+        $comment->delete();
+        session()->flash('message', 'Comment deleted Successfully .');
     }
 
     public function render()
     {
-        return view('livewire.comments');
+        return view('livewire.comments', [
+            'comments' => Comment::latest()->paginate(2)
+        ]);
     }
 }
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -42,6 +42,7 @@
                 <p class="text-gray-800">{{ $comment->body }}</p>
             </div>
         @endforeach
+        {{ $comments->links() }}
 
 
     </div>
